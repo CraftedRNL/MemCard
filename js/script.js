eb276c782dbe375fe76d594fe07814529c70a1d4
Ryan <126Rliu@frhsd.com>
2025-03-27 14:00:01 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const cards = document.querySelectorAll(".card");
+
+let cardOne, cardTwo;
+
+function flipCard(e){
+    let clickedCard = e.target;
+    if(clickedCard !== cardOne){
+        clickedCard.classList.add("flip");
+    if(!cardOne){
+        return cardOne = clickedCard;
+    }
+    cardTwo = clickedCard;
+
+    let cardOneImg = cardOne.querySelector("img"),
+    cardTwoImg = cardOne.querySelector("img");
+    matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2){
+    if(img1 === img2){
+        return console.log("Cards matched")
+    }
+    setTimeout(()=>{
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    },400)
+}
+
+cards.forEach(card =>{
+    card.addEventListener("click", flipCard)
+});
